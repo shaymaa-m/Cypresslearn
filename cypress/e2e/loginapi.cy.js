329fe3f42f1c0ca0c88a2daca14a0d6b9a805b14
shaymaam <shaymaa@cialfo.com.sg>
2023-01-21 11:23:57 +0500

    changes to the file

--- cypress/e2e/loginapi.cy.js
+++ cypress/e2e/loginapi.cy.js
@@ -1,16 +1,16 @@
-cy.request({
-    method: 'POST',
-    url: 'https://sandbox.cialfo.sg/v3/sessions?access_token=Zx2EE58K0tyzL8V4Xh7GLYYhQzHcz7Q05jrRXAcfw1&locale=en-US&captcha_token=',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    },
-    body: {
-        email: 'shaymaa@cialfo.com.sg',
-        password: '12345678'
-    }
-}).then((response) => {
-    expect(response.status).to.eq(200);
-    expect(response.body).to.have.property('tokZx2EE58K0tyzL8V4Xh7GLYYhQzHcz7Q05jrRXAcfw1en');
-    expect(response.headers).to.have.property('content-type', 'application/json');
+it('checks api status successful login', () => {
+    cy.request({
+        method: 'POST',
+        url: 'https://sandbox.cialfo.sg/v3/sessions?access_token=Zx2EE58K0tyzL8V4Xh7GLYYhQzHcz7Q05jrRXAcfw1&locale=en-US&captcha_token=',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: {
+            email: 'shaymaa@cialfo.com.sg',
+            password: '12345678'
+        }
+    }).then((response) => {
+        expect(response.status).to.eq(400);
+    });
 });
