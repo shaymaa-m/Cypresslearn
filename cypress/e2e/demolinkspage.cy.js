7a0e9d28b51b5d17d1e37b827b24f9309812c9cc
shaymaam <shaymaa@cialfo.com.sg>
2023-03-16 15:56:00 +0500

    Demo link locator removed spaces

--- cypress/e2e/demolinkspage.cy.js
+++ cypress/e2e/demolinkspage.cy.js
@@ -8,23 +8,16 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 describe('adding all types of selectors', () => {
    
-
     it('open page link', () => {
         cy.visit('https://demoqa.com/links');
     })
-    
     it('get element by id', () => {
-
         cy.get('#simpleLink').contains('Home').should('be.visible')
     })
-    
-    it('get element by LinkText', () => {
-         
+    it('get element by LinkText', () => {    
         cy.contains('Forbidden').should('be.visible')
     })
-
-    it('get element by class', () => {
-         
+    it('get element by class', () => {     
         cy.get('.main-header').contains('Links')
     })
 })
