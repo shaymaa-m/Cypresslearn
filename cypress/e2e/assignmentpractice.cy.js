c95e71a22f1f4ddfc830c8c89728325cb848fb22
shaymaam <shaymaa@cialfo.com.sg>
2023-04-05 15:33:36 +0500

    adding changes to windows and iframes

--- cypress/e2e/assignmentpractice.cy.js
+++ cypress/e2e/assignmentpractice.cy.js
@@ -23,24 +23,21 @@ describe('trying to implement the stuff learned', () => {
     })
     it('working with windows or origin', () => {
         cy.visit("https://ultimateqa.com/button-success")
-        /*cy.window().then((win) => {
-            cy.stub(win,'open', url => {
-                win.location.href='https://www.facebook.com/login.php?skip_api_login=1&api_key=966242223397117&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fshare.php%3Fu%3Dhttps%253A%252F%252Fultimateqa.com%252Fbutton-success%252F%253Futm_source%253Dfacebook%2526utm_medium%253DSocial%2526utm_campaign%253DSocialWarfare&cancel_url=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Fclose_window%2F%3Fapp_id%3D966242223397117%26connect%3D0%23_%3D_&display=popup&locale=en_GB';
-                }).as("win")*/
-            cy.wait(5000)
-            cy.get('.entry-content > .swp_social_panel > .swp_facebook > .nc_tweet').click()
-            //alternate solution was trying out origin method
-            cy.origin('https://www.facebook.com/login.php?skip_api_login=1&api_key=966242223397117&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fshare.php%3Fu%3Dhttps%253A%252F%252Fultimateqa.com%252Fbutton-success%252F%253Futm_source%253Dfacebook%2526utm_medium%253DSocial%2526utm_campaign%253DSocialWarfare&cancel_url=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Fclose_window%2F%3Fapp_id%3D966242223397117%26connect%3D0%23_%3D_&display=popup&locale=en_GB',()=> {
-            cy.get('h2').should('have.text', 'Facebook')
+        cy.get('.entry-content > .swp_social_panel > .swp_facebook > .nc_tweet')
+        .invoke('removeAttr', 'target')
+        .click()
+        .wait(1000)
+        // trying out origin method //
+        cy.origin('https://www.facebook.com/share.php',()=> {
+        cy.url().should('contain', 'share.php')
+        cy.get('.signup_box_content > ._42ft').contains('Sign Up')
+           })
+          })  
        })
-    })
-    it('working with iframes', () => {
+    it.only('working with iframes', () => {
         cy.visit('https://www.globalsqa.com/demo-site/frames-and-windows/#iFrame')
-        cy.frameLoaded()//loads five frames still not locating iframe with given id tried class but same error
+        cy.frameLoaded('.lazyloaded[name="globalSqa"]')//loads five frames still not locating iframe with given id tried class but same error
         cy.wait(5000)
-        cy.iframe('#aswift_0').find('.mobile_menu_toggler').should('be.visible').click()
-        //cy.get('.resp-tab-content-active')
-        //cy.iframe('.resp-tab-content-active').find('.mobile_menu_toggler').should('be.visible').click()
+        cy.iframe('.lazyloaded[name="globalSqa"]').find('#mobile_menu_toggler').click()
+        cy.iframe('.lazyloaded[name="globalSqa"]').find('#mobile_menu').contains('CheatSheets').click()
     })
-
-})
